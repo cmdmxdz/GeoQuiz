2e7c097e1b9a0ebbc8b85a6314bbbb043ef0eb5f
cmdmxdz <xdz_coder@163.com>
2022-10-28 11:51:13 +0800

    [Modified]1.7 创建提示消息 点击按钮，弹出默认toast提示

--- app/src/main/java/com/dy/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/dy/geoquiz/QuizActivity.kt
@@ -1,11 +1,28 @@
 package com.dy.geoquiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class QuizActivity : AppCompatActivity() {
+
+    private var mTrueButton: Button? = null
+    private var mFalseButton: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+
+        mTrueButton = findViewById(R.id.true_button)
+        mTrueButton?.setOnClickListener {
+            // Does nothing yet, but soon!
+            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+        }
+        mFalseButton = findViewById(R.id.false_button);
+        mFalseButton?.setOnClickListener {
+            // Does nothing yet, but soon!
+            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+        }
     }
 }
