98f72c2ec4e81b5d8cb9ce5fb7692992547a3b05
cmdmxdz <xdz_coder@163.com>
2022-10-28 12:01:29 +0800

    [Modified]1.11 定制toast消息，改在屏幕顶部而不是底部显示弹出消息 当前版本为sdk=32>30, 重力位置设置不生效

--- app/src/main/java/com/dy/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/dy/geoquiz/QuizActivity.kt
@@ -1,6 +1,7 @@
 package com.dy.geoquiz
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -17,12 +18,22 @@ class QuizActivity : AppCompatActivity() {
         mTrueButton = findViewById(R.id.true_button)
         mTrueButton?.setOnClickListener {
             // Does nothing yet, but soon!
-            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            val toast = Toast(this)
+            /// 当前版本为sdk=32>30, 重力位置设置不生效
+            toast.setGravity(Gravity.TOP, 0, 0)
+            toast.setText(R.string.correct_toast)
+            toast.duration = Toast.LENGTH_SHORT
+            toast.show()
         }
         mFalseButton = findViewById(R.id.false_button);
         mFalseButton?.setOnClickListener {
             // Does nothing yet, but soon!
-            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+            val toast = Toast(this)
+            /// 当前版本为sdk=32>30, 重力位置设置不生效
+            toast.setGravity(Gravity.TOP, 0, 0)
+            toast.setText(R.string.incorrect_toast)
+            toast.duration = Toast.LENGTH_SHORT
+            toast.show()
         }
     }
 }
